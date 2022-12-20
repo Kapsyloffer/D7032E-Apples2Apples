fb1a14174aa713af02f8f42a52921752e04a9e3e
Christoffer Lindkvist <crillemega@gmail.com>
2022-12-20 02:33:15 +0100

    Removed OG code cause errors

---
+++ src/player.rs
@@ -0,0 +1,19 @@
+struct player
+{
+    player_ID : i32,
+    is_bot : bool,
+    online : bool,
+    //connection : socket,
+    //inFromClient : BufferedReader,
+    //outToClient : DataOutputStream,
+    hand : Vec,
+    green_apples : Vec,
+}  
+
+impl player
+{
+    fn foo() -> i32
+    {
+        return 0;
+    }
+}
